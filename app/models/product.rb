288e932e828746bdd8b1cce060770663053e19ff
sanmingzi <zhiming.zhong@outlook.com>
2021-04-08 15:25:04 +0800

    add validates for product; add fixtures of products; add model test for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :title, presence: true, uniqueness: {case_sensitive: true}
+  validates :description, presence: true
+  validates :image_url, presence: true, format: {
+    with: /.(gif|jpg|png)/,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,44 @@
+# bundle exec rails test:models
+
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  def new_product(title: 'another title', description: 'another description', image_url: 'another.jpg', price: 49.0)
+    Product.new(title: title, description: description, image_url: image_url, price: price)
+  end
+
+  test 'product attributes must not be empty' do
+    Product.new.tap do |p|
+      assert p.invalid?
+      [:title, :description, :image_url, :price].each do |attr|
+        assert p.errors[attr].any?
+      end
+    end
+  end
+
+  test 'product title must be unique' do
+    new_product(title: 'Ruby Metaprogramming').tap do |p|
+      assert p.invalid?
+      assert_equal ['has already been taken'], p.errors[:title]
+    end
+  end
+
+  test 'product image_url must be gif|jpg|png' do
+    ['ruby.gif', 'ruby.jpg', 'ruby.png'].each do |image_url|
+      new_product(image_url: image_url).tap { |p| assert p.valid?, "#{image_url} shouldn't be invalid" }
+    end
+    ['ruby.more', 'ruby', 'ruby.other'].each do |image_url|
+      new_product(image_url: image_url).tap { |p| assert p.invalid?, "#{image_url} shouldn't be valid" }
+    end
+  end
+
+  test 'product price must be positive' do
+    new_product.tap { |p| assert p.valid? }
+    new_product(price: 0).tap { |p|
+      assert p.invalid?
+      assert_equal p.errors[:price], ['must be greater than or equal to 0.01']
+    }
+  end
 end
