337051b36be22fa845981846245ab31cfc7e5019
sanmingzi <zhiming.zhong@outlook.com>
2021-04-09 17:46:21 +0800

    support empty cart; show total price in cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -50,8 +50,9 @@ class CartsController < ApplicationController
   # DELETE /carts/1 or /carts/1.json
   def destroy
     @cart.destroy
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: "Cart was successfully destroyed." }
+      format.html { redirect_to store_index_url, notice: "Your Cart is empty now." }
       format.json { head :no_content }
     end
   end
