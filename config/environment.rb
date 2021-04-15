d422ad8c070ae09f6e755ba087e9c6c62794acf0
sanmingzi <zhiming.zhong@outlook.com>
2021-04-15 19:52:43 +0800

    use docker to deploy

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.active_record.cache_versioning = false
   config.cache_store = :redis_store, (ENV['REDIS_CACHE_URL'] || '').split(','), {expires_in: 90.minutes}
   config.session_store :redis_store, servers: (ENV['REDIS_SESSION_URL'] || '').split(','), expire_after: 90.minutes, key: '_wk_session'
 end
